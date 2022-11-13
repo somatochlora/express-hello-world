789a1a48baed628c3b7aecfc9f456879de86d7f6
Reuven Martin <jaeger33@gmail.com>
2022-11-13 16:12:29 -0500

    add eBird code

--- app.js
+++ app.js
@@ -1,59 +1,79 @@
-const express = require("express");
-const app = express();
-const port = process.env.PORT || 3001;
+const fs = require('fs');
+const cron = require('node-cron');
 
-app.get("/", (req, res) => res.type('html').send(html));
+const eBirdKey = process.env.EBIRD_API_KEY;
+const ntfyURL = process.env.NTFY_URL;
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+async function fetchEbirdResults (location) {
+  let results = await fetch("https://api.ebird.org/v2/product/lists/" + location + "?key=" + eBirdKey + "&maxResults=200");
+  return await results.json();
+}
 
-
-const html = `
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Hello from Render!</title>
-    <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
-    <script>
-      setTimeout(() => {
-        confetti({
-          particleCount: 100,
-          spread: 70,
-          origin: { y: 0.6 },
-          disableForReducedMotion: true
+cron.schedule('0 * * * *', () => {
+  let config = JSON.parse(fs.readFileSync('config.json'));
+  let lastResults = JSON.parse(fs.readFileSync('savedResults.json'));
+  
+  let locations = config.locations;
+  let savedChecklists = {}
+  let toEmail = [];
+  let promisesArray = [];
+  
+  for (let location of locations) {
+    let curLocationToSave = {
+        checklists:[]
+    }
+    savedChecklists[location.id] = curLocationToSave;
+  
+    let eBirdResults = fetchEbirdResults(location.id)
+    promisesArray.push(eBirdResults);
+    eBirdResults.then(results => {
+        results.forEach(checklist => {
+           curLocationToSave.checklists.push(checklist.subId); 
         });
-      }, 500);
-    </script>
-    <style>
-      @import url("https://p.typekit.net/p.css?s=1&k=vnd5zic&ht=tk&f=39475.39476.39477.39478.39479.39480.39481.39482&a=18673890&app=typekit&e=css");
-      @font-face {
-        font-family: "neo-sans";
-        src: url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff2"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("opentype");
-        font-style: normal;
-        font-weight: 700;
-      }
-      html {
-        font-family: neo-sans;
-        font-weight: 700;
-        font-size: calc(62rem / 16);
-      }
-      body {
-        background: white;
-      }
-      section {
-        border-radius: 1em;
-        padding: 1em;
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        margin-right: -50%;
-        transform: translate(-50%, -50%);
-      }
-    </style>
-  </head>
-  <body>
-    <section>
-      Hello from Render!
-    </section>
-  </body>
-</html>
-`
+  
+        if (lastResults.hasOwnProperty(location.id)) {
+            results = results.filter(checklist => {
+                return lastResults[location.id].checklists.indexOf(checklist.subId) == -1
+            });
+        }
+  
+        results.forEach(checklist => {
+            toEmail.push(checklist);
+        });
+    });
+  }
+  
+  Promise.all(promisesArray).then(() => {
+    if (toEmail.length > 0) {
+        console.log("new results found");
+        let allOutput = ""
+        toEmail.forEach(checklist => {
+            let output = 
+                "Location: " +
+                checklist.loc.name +        
+                " Observer: " +
+                checklist.userDisplayName +
+                ", " +
+                checklist.numSpecies +
+                " species observed, " +
+                checklist.obsDt +
+                " " +
+                checklist.obsTime +
+                " https://ebird.org/checklist/" +
+                checklist.subId;
+            console.log(output);
+            allOutput += output + "\n";
+        });
+        fetch(
+            'http://ntfy.sh/reuven-ebird-alerts',
+            {
+                method: 'POST',
+                body: allOutput
+            }
+        );
+    }
+    
+    fs.writeFileSync('savedResults.json', JSON.stringify(savedChecklists, null, 2));
+  });
+});
+
